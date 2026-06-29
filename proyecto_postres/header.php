c9a9b0f859c59edfe72c609a6949be0cf7e5c437
ana-donaldson <anadonaldson700@gmail.com>
2026-06-29 19:15:27 -0300

    Update header.php

--- proyecto_postres/header.php
+++ proyecto_postres/header.php
@@ -1,19 +1,16 @@
-
-<header>
-    <h1>🍰 Postesión</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="recetas.php">Recetas</a></li>
-            <li><a href="ruleta.php">Ruleta</a></li>
-            <?php if(isset($_SESSION['usuario_id'])): ?>
-                <li><a href="perfil.php">👤 <?php echo $_SESSION['nombre_usuario']; ?></a></li>
-                <li><a href="subir_receta.php">➕ Subir receta</a></li>
-                <li><a href="logout.php">🚪 Salir</a></li>
-            <?php else: ?>
-                <li><a href="login.php">🔐 Iniciar sesión</a></li>
-                <li><a href="registro.php">📝 Registrarse</a></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
-</header>
+<nav>
+  <a href="index.php" class="logo">Dulce<span>Tentación</span></a>
+  <ul>
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="recetas.php">Recetas</a></li>
+    <li><a href="ruleta.php">Ruleta</a></li>
+    <?php if(isset($_SESSION['usuario_id'])): ?>
+      <li><a href="perfil.php">👤 <?php echo htmlspecialchars($_SESSION['nombre_usuario']); ?></a></li>
+      <li><a href="subir_receta.php" class="btn" style="padding:.4rem 1rem;font-size:.78rem;">+ Subir receta</a></li>
+      <li><a href="logout.php">Salir</a></li>
+    <?php else: ?>
+      <li><a href="login.php">Ingresar</a></li>
+      <li><a href="registro.php" class="btn" style="padding:.4rem 1rem;font-size:.78rem;">Registrarse</a></li>
+    <?php endif; ?>
+  </ul>
+</nav>
